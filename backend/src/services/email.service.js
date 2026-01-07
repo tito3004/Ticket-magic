75e2adaa5ab68eac127ed59ed6f86d9b4936d291
tito3004 <tito20022004@outlook.es>
2026-01-07 18:12:13 -0500

    correccion

--- backend/src/services/email.service.js
+++ backend/src/services/email.service.js
@@ -7,11 +7,15 @@ const transporter = nodemailer.createTransport({
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
-  }
+  },
+  
+  tls: {
+    rejectUnauthorized: false, // 🔑 evita errores en Render
+  },
 });
 //Servicio: enviar correos
 exports.sendMail = async (to, subject, htmlContent, attachmentPath = null) => {
- /**  const mailOptions = {
+ const mailOptions = {
     from: `"Ticketmagic" <${process.env.EMAIL_USER}>`,
     to,
     subject,
@@ -29,7 +33,7 @@ exports.sendMail = async (to, subject, htmlContent, attachmentPath = null) => {
   }
 
   return transporter.sendMail(mailOptions);
-  */
+  
 };
 //Servicio: enviar correos de notificacion de acceso indebidos al administrador
 exports.sendMailAdminNotificacion = async (req, res) => {
